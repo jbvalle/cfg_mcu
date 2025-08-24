402e55d501a07eab751399443c185f9819303617
John Bryan Valle <jbvalle.jv@gmail.com>
2025-08-24 18:20:01 +0200

    test: set stm32f401re

--- cfg_mcu.h
+++ cfg_mcu.h
@@ -1,8 +1,8 @@
 #ifndef CFG_MCU_H
 #define CFG_MCU_H
 
-//#define MCU_STM32F401RE
-#define MCU_STM32F429ZI
+#define MCU_STM32F401RE
+//#define MCU_STM32F429ZI
 
 // MCU System Clock AHB
 #define MCU_SYSTEM_CLK 16000000u
