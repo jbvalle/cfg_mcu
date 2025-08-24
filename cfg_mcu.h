1df6892afd0f9cac0e00a4b5b6cd03867ecd49e8
John Bryan Valle <jbvalle.jv@gmail.com>
2025-08-24 19:27:42 +0200

    added cortexm4 to mcu

--- cfg_mcu.h
+++ cfg_mcu.h
@@ -1,8 +1,10 @@
 #ifndef CFG_MCU_H
 #define CFG_MCU_H
+#include "cortex_m4.h"
 
-#define MCU_STM32F401RE
+//#define MCU_STM32F401RE
 //#define MCU_STM32F429ZI
+#define MCU_STM32F407VGT6
 
 // MCU System Clock AHB
 #define MCU_SYSTEM_CLK 16000000u
